9ef74b906ad9aa7f4150a7f45bf9c21529a6763e
Victor Nascimento de Melo <victornascimentobr48@gmail.com>
2024-10-17 10:58:23 -0300

    Delete Exercicios-JS/JavaScript directory

--- Exercicios-JS/JavaScript/ex004.js
+++
@@ -1,9 +0,0 @@
-// </> Programa Bom Dia!
-// Este programa trabalha com o conceito de varável. Nele o usuário vai passar uma informação para o programa, 
-// o programa guarda em uma variável e devolve uma resposta baseada nesta varável. O desafio é o seguinte, escreve 
-// um código que recebe o nome de uma pessoa e escreve "Bom Dia!". Por exemplo, se a entrada do programa 
-// for "João" o sua resposta deve ser "Bom Dia João!"
-
-var nome, nome2
-nome = prompt('Qual é seu nome? ')
-console.log(`Bom Dia ${nome}!`)
