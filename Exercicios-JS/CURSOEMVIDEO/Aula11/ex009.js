eb34595d9396ed8529461c7a04f17dd68b7acf21
Victor Nascimento de Melo <victornascimentobr48@gmail.com>
2024-10-17 13:01:24 -0300

    Delete Exercicios-JS/CURSOEMVIDEO directory

--- Exercicios-JS/CURSOEMVIDEO/Aula11/ex009.js
+++
@@ -1,7 +0,0 @@
-var país = 'Brasil'
-
-if (país == 'Brasil'){
-    console.log('Brasileiro!')
-}else {
-    console.log('Estrangeiro!')
-}
--- Exercicios-JS/CURSOEMVIDEO/JavaScript/ex001.js
+++
@@ -1,8 +0,0 @@
-// </> É preciso praticar muito!
-
-//Vamos começar a lista praticando o comando de saída de dados. Lembre que o comando de saida é alert(). 
-//Veja que ao lado do enunciado existe um quadro informando quais são as entradas e quais as saídas do programa. 
-//Sua missão é escrever um programa para exibir exatamente a mensagem: 
-//"Eh preciso praticar muito para aprender a programar!". Note que esse programa não tem entradas.
-
-console.log('Eh preciso praticar muito para aprender a programar!');
