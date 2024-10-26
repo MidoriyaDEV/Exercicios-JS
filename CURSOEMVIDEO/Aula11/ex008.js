84fa1537d73dd93f7123736c1aaef8e720b19d13
Victor Nascimento de Melo <victornascimentobr48@gmail.com>
2024-10-26 09:05:56 -0300

    Update✅

--- CURSOEMVIDEO/Aula11/ex008.js
+++
@@ -1,9 +0,0 @@
-var vel = 60.5
-
-console.log(`A velocidade do seu carro é ${vel}km/h`)
-if (vel >60) {
-    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
-}else {
-    console.log('Dirija sempre usando sinto de segurança!')
-}
-
